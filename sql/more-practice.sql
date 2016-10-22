e56a5f38a7b010d631f146bd65b38915e574209f
Christy Chen <christychen126@gmail.com>
2016-10-22 20:12:34 +0000

    SQL done

---
+++ sql/more-practice.sql
@@ -0,0 +1,23 @@
+-- Include your solutions to the More Practice problems in this file.
+
+
+
+-- INSERT
+
+INSERT INTO models (year, brand_name, name)  VALUES (2015, 'Chevrolet', 'Malibu');
+INSERT INTO models (year, brand_name, name) VALUES (2015, 'Subaru', 'Outback');
+
+
+-- CREATE TABLE
+
+CREATE TABLE awards(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    year INTEGER NOT NULL,
+    winner_id VARCHAR(50) NULL 
+);
+
+-- More INSERT
+
+INSERT INTO awards (name, year, winner_id) VALUES ('IIHS Safety Award', 2015, 49);
+INSERT INTO awards (name, year, winner_id) VALUES ('IIHS Safety Award', 2015, 59);
